8a08d06ab187e6337427b86b561cfbba775abb97
ghostrabbir0703 <rummypa123@gmail.com>
2026-08-09 19:26:48 -0400

    añadiendo las rutas de la api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,32 @@
 <?php
 
-use Illuminate\Http\Request;
+//use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
+
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\TagController;
+use App\Http\Controllers\TaskController;
+
+
+// Categories
+Route::apiResource('categories', CategoryController::class);
+
+
+// Tags
+Route::apiResource('tags', TagController::class);
+
+
+// Tasks
+Route::apiResource('tasks', TaskController::class);
+
+
+// Toggle task completed
+Route::patch(
+    'tasks/{task}/toggle-completed',
+    [TaskController::class, 'toggleCompleted']
+);
+/* Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+ */
